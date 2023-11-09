d8f886120e54871a508991ee07d632dbf29a2838
PRTGRWL <100923187+PRTGRWL@users.noreply.github.com>
2023-11-09 15:01:51 +0530

    Update script_2.pl

--- flanking_ssr_scripts/script_2.pl
+++ flanking_ssr_scripts/script_2.pl
@@ -8,7 +8,6 @@ use warnings;
 # Contact : prtgrwl8@gmail.com
 #USAGE : perl script_2.pl                                                         
 
-
 my @abc;
 my %all_identifier;
 my @all_result;
@@ -16,14 +15,13 @@ my @all_result;
 open (FH,"final") or die 'error';
     while (<FH>){
         chomp;
-       
-	@abc= split(/\t/,$_);
-	#Store all informations
-   	if ($abc[3]==1) {
-        	push (@all_result, $_);
-         	$all_identifier{$abc[0]}=1;
-    	}
 
+        @abc= split(/\t/,$_);
+        #Store all informations
+        if ($abc[3]==1) {
+                push (@all_result, $_);
+                $all_identifier{$abc[0]}=1;
+        }
     }
 
 # process unique file
@@ -31,19 +29,19 @@ my $pattern_len1;
 
 foreach my $key (keys %all_identifier){
         #print "$key\n";
-	my $pat="N"; my $idf=0; my $str="M";
-	foreach (@all_result){
-    	my @split_val = split(/\t/);
-    		if ($split_val[0] == $key) {
-		my $pat_len = length ($pat);
-		my $str_len = length ($split_val[2]);
-			if ($str_len > $pat_len) {
-				$pat = $split_val[2];
-				$idf = $split_val[0];
-				$str = $split_val[1];		
-			}
-    		}
-	}
+        my $pat="N"; my $idf=0; my $str="M";
+        foreach (@all_result){
+        my @split_val = split(/\t/);
+                if ($split_val[0] == $key) {
+                my $pat_len = length ($pat);
+                my $str_len = length ($split_val[2]);
+                        if ($str_len > $pat_len) {
+                                $pat = $split_val[2];
+                                $idf = $split_val[0];
+                                $str = $split_val[1];
+                        }
+                }
+        }
 my $flen=length($pat);
 print "$idf\t$str\t$pat\t$flen\n";
 }
